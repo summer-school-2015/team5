7ee0300c96fc77795f2fd7bb11d662f8c5a4d8fd
nkmashaev <nikita_mashaev@mail.ru>
2015-06-02 13:23:31 +0300

    HelloWorld

---
+++ com/team5/0-helloex/Hello.java
@@ -0,0 +1,6 @@
+package com.team5.0-helloex;
+class Hello{
+	public static void main(String[] args){
+		Printer.printHello("HelloWorld");
+	}
+}
---
+++ com/team5/0-helloex/Printer.java
@@ -0,0 +1,6 @@
+package com.team5.0-helloex;
+class Printer{
+	public static void printHello(String arg){
+		System.out.println(arg);
+	}
+}
