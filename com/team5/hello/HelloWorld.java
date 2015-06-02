01c5ddad895ea18ba64dfe603e6ca85f6549a441
nkmashaev <nikita_mashaev@mail.ru>
2015-06-02 13:11:01 +0300

    Second commit

---
+++ com/team5/hello/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.team5.hello;
+class HelloWorld{
+	public static void main(String[] args){
+		Printer.printHello("HelloWorld");
+	}
+}
---
+++ com/team5/hello/Printer.java
@@ -0,0 +1,6 @@
+package com.team5.hello;
+class Printer{
+	public static void printHello(String arg){
+		System.out.println(arg);
+	}
+}
