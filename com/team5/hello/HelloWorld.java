e64185c9c451e821cc57a730d2f5911bdf155bd7
nkmashaev <nikita_mashaev@mail.ru>
2015-06-02 13:33:45 +0300

    Remove Dirs

--- com/team5/hello/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package com.team5.hello;
-class HelloWorld{
-	public static void main(String[] args){
-		Printer.printHello("HelloWorld");
-	}
-}
--- com/team5/hello/Printer.java
+++
@@ -1,6 +0,0 @@
-package com.team5.hello;
-class Printer{
-	public static void printHello(String arg){
-		System.out.println(arg);
-	}
-}
